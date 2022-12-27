d838583c5183bc28829a1f602fcf36d499a48dcb
Avin Lanson Tharakan <avinlanson@gmail.com>
2022-12-26 22:08:58 -0500

    added show, delete, update feature. Also switches to mysql2

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const app = express()
+const app = express();
+const mysql = require("mysql2");
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "password",
-    databse: "employesSystem",
+    database: "sys",
 })
 
 
 app.post('/create', (req, res) =>{
+    console.log("hi2");
     const name = req.body.name
     const age = req.body.age
     const country = req.body.country
@@ -22,11 +28,49 @@ app.post('/create', (req, res) =>{
         if (err) {
             console.log(err)
         }else{
-            console.log("Values added")
+            res.send("Values added")
         }
     });
 })
 
+app.get('/employees', (req,res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query(
+      "UPDATE employees SET wage = ? WHERE id = ?",
+      [wage, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+        console.log(err);
+    } else {
+        res.send(result);
+    }
+    });
+});
+  
 app.listen(3001, ()=>{
     console.log("Hello World!")
 } )
